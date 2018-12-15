ed90f3c9b3741917370f7f4785af44713c4dbcd5
Kwonsy <funsumer@gmail.com>
2018-12-16 04:14:57 +0900

    알파버전 v1.1

---
+++ app/src/main/java/honbab/voltage/com/task/VersionTask.java
@@ -0,0 +1,112 @@
+package honbab.voltage.com.task;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
+
+import org.json.JSONObject;
+
+import honbab.voltage.com.tete.R;
+import honbab.voltage.com.tete.Statics;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+public class VersionTask extends AsyncTask<Void, Void, Void> {
+    private Context mContext;
+    private OkHttpClient httpClient;
+
+    String version, status, text;
+    String appVersion;
+
+    public VersionTask(Context mContext, OkHttpClient httpClient) {
+        this.mContext = mContext;
+        this.httpClient = httpClient;
+    }
+
+    @Override
+    protected void onPreExecute() {
+
+    }
+
+    @Override
+    protected Void doInBackground(Void... objects) {
+        FormBody body = new FormBody.Builder()
+                .add("opt", "version")
+                .add("device", "android")
+                .build();
+
+        Request request = new Request.Builder().url(Statics.opt_url).post(body).build();
+
+        try {
+            okhttp3.Response response = httpClient.newCall(request).execute();
+            if (response.isSuccessful()) {
+                String bodyStr = response.body().string();
+                JSONObject obj = new JSONObject(bodyStr);
+                Log.e("abc", "Login root = " + obj);
+
+                version = obj.getString("version");
+                status = obj.getString("status");
+                text = obj.getString("text");
+            } else {
+                    Log.d("abc", "Error : " + response.code() + ", " + response.message());
+            }
+
+        } catch (Exception e) {
+            Log.e("abc", "Error : " + e.getMessage());
+            e.printStackTrace();
+        }
+
+        try {
+            PackageInfo pInfo = mContext.getPackageManager().getPackageInfo(mContext.getPackageName(), 0);
+            appVersion = pInfo.versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Void result) {
+//        super.onPostExecute(result);
+            Log.e("abc", "version" + version);
+            Log.e("abc", "appVersion" + appVersion);
+
+            if (version.equals(appVersion)) {
+
+            } else {
+                AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+//        builder.setTitle("AlertDialog Title");
+                builder.setMessage(text);
+                builder.setPositiveButton(R.string.update,
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                final String appPackageName = mContext.getPackageName(); // getPackageName() from Context or Activity object
+                                try {
+                                    mContext.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + appPackageName)));
+                                } catch (android.content.ActivityNotFoundException anfe) {
+                                    mContext.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + appPackageName)));
+                                }
+                            }
+                        });
+//                builder.setNegativeButton(R.string.reserve_new_godmuk,
+//                        new DialogInterface.OnClickListener() {
+//                            public void onClick(DialogInterface dialog, int which) {
+//                                Toast.makeText(getActivity().getApplicationContext(), R.string.make_new_godmuk, Toast.LENGTH_LONG).show();
+//                                Intent intent = new Intent(getActivity(), ReservActivity.class);
+//                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//                                getActivity().startActivity(intent);
+//                            }
+//                        });
+                builder.show();
+            }
+    }
+
+}
